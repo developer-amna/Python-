aa3c89aaaafb9808cb90162ec5cecbca045e3b67
developer Amna <hafizaamna.webdev@gmail.com>
2025-08-13 16:52:51 +0500

    python

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-# Basic hello-world code 
+# 1. Basic hello-world code 
 
 print ("Hello World")
+
+# 2.Variables and data types
+
+name = "Amna"
+age = 20
+height = 5.0
+is_student = True
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# 3. Arithmetic operatiors
+
+a = 15
+b = 5
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+
+# 4. Comparison Operators
+
+x = 10
+y = 20
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
